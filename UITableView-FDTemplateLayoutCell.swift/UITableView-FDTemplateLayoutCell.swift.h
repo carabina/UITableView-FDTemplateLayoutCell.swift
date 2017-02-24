e3cc71a337001445de1905952ccd10ee1207fd04
huangboju <529940945@qq.com>
2017-02-24 16:29:03 +0800

    add fromwork

---
+++ UITableView-FDTemplateLayoutCell.swift/UITableView-FDTemplateLayoutCell.swift.h
@@ -0,0 +1,19 @@
+//
+//  UITableView-FDTemplateLayoutCell.swift.h
+//  UITableView-FDTemplateLayoutCell.swift
+//
+//  Created by 伯驹 黄 on 2017/2/24.
+//  Copyright © 2017年 伯驹 黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UITableView-FDTemplateLayoutCell.swift.
+FOUNDATION_EXPORT double UITableView_FDTemplateLayoutCell_swiftVersionNumber;
+
+//! Project version string for UITableView-FDTemplateLayoutCell.swift.
+FOUNDATION_EXPORT const unsigned char UITableView_FDTemplateLayoutCell_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UITableView_FDTemplateLayoutCell_swift/PublicHeader.h>
+
+
